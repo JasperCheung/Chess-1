33ef7332f50e1a536021223738f8a9c69d3c242e
kchen29 <kchen29@stuy.edu>
2017-01-12 11:47:56 -0500

    completed Version 0 and renamed driver to woo

--- Chess.java
+++ Chess.java
@@ -14,14 +14,46 @@ public class Chess {
     }
     // create 1 piece on board
     private void setBoardPiece(int r, int c) {
-	board[r][c] = new Queen(true);
+	Piece p = null;
+        
+	if (r == 0 || r == 7) {
+	    boolean color = r == 7; //white if r == 7
+	    if (c == 0 || c == 7)
+		p = new Rook(color);
+	    if (c == 1 || c == 6)
+		p = new Knight(color);
+	    if (c == 2 || c == 5)
+		p = new Bishop(color);
+	    if (c == 3)
+		p = new Queen(color);
+	    if (c == 4)
+		p = new King(color);
+	}
+	if (r == 1)
+	    p = new Pawn(false);
+	if (r == 6)
+	    p = new Pawn(true);
+	
+	if (p != null)
+	    board[r][c] = p;
     }
     public void printBoard() {
-	for (Piece[] row : board) {
-	    for (Piece p : row) {
-		System.out.print(p);
-	    } 
-	    System.out.println();
+	for (int r = 0; r < 8; r++) {
+	    String s = "";
+	    for (int c = 0; c < 8; c++) {
+		Piece p = board[r][c];
+		if (p != null) {
+		    s += p + " ";
+		} else {
+		    //put white boxes ■ for empty squares
+		    if ((r + c) % 2 == 0) {
+		        s += "■ ";
+		    } else {
+			s += "  ";
+		    }
+		}
+	    }
+	    System.out.println(s);
 	}
     }
 }
--- Woo.java
+++ Woo.java
@@ -1,4 +1,4 @@
-public class Driver {
+public class Woo {
     public static void main(String[] args) {
 	Queen q = new Queen(true);
 	System.out.println(q);
